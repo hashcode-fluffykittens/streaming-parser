eebd9af093f81ce1a7a2b13fb06eeebb715f259a
Daniel Mateus Pires <dmateusp@gmail.com>
2017-02-23 20:29:14 +0000

    Adding scorer

---
+++ scorer.py
@@ -0,0 +1,12 @@
+# videos, endpoints, requests
+submission = """
+	
+"""
+def is_valid(submission):
+	submission_lines = submission.splitlines():
+	n_cache_server_used = submission_lines[0]
+	for line in submission_lines[1:]:
+		
+
+def get_score(submission):
+	submission_lines = submission.splitlines():
